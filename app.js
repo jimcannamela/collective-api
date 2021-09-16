89be4fe3aba060575337fd02447255283f2c8fcd
ChaoticNeutralCode <rich@busyrich.com>
2021-09-16 13:38:38 -0700

    Add OpenAPI docs generator and endpoint.

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path')
 const logger = require('morgan')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const swaggerUi = require('swagger-ui-express')
+const apiDocs = require(`./openapi-gen`)
 
 const index = require('./app/index')
 const people = require('./app/people/routes')
@@ -24,6 +26,7 @@ if (process.env.NODE_ENV !== 'test') {
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors())
+app.use('/openapi', swaggerUi.serve, swaggerUi.setup(apiDocs));
 
 app.use((req, res, next) => {
   if (req.query.delay) return setTimeout(next, parseInt(req.query.delay, 10))
