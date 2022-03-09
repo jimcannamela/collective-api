d3cc003e71980642ef155d16e1c731ba23807d99
David Grieser <david.grieser@galvanize.com>
2022-03-09 14:36:07 -0600

    finish CRUD of /todos

--- app/todos/routes.js
+++ app/todos/routes.js
@@ -7,4 +7,37 @@ router.get('/todos', (req, res) => {
     res.json(todos);
 })
 
+router.get('/todos/:id', (req, res) => {
+    const todo = db.todos.find(req.params.id)
+    res.json(todo);
+})
+
+router.post('/todos', (req, res) => {
+    const newTodo = db.todos.insert({
+        name: req.body.name,
+        complete: req.body.complete || false
+    })
+    res.json(newTodo)
+})
+
+router.patch('/todos/:id', (req, res) => {
+    const todo = db.todos.find(req.params.id)
+    if(req.body.name) {
+        todo.name = req.body.name
+    }
+    if(req.body.complete) {
+        todo.complete = req.body.complete
+    }
+    res.json(todo)
+})
+
+router.delete('/todos/:id', (req, res) => {
+    const todo = db.todos.find(req.params.id)
+    if(todo) {
+        db.todos.delete(req.params.id)
+    }
+
+    res.status(200)
+    res.end()
+})
 module.exports = router;
